da6be5b1c8f60624bc00732a08448c5072039a2a
soureshmondal <117149085+soureshmondal@users.noreply.github.com>
2025-11-13 22:41:15 +0530

    Update apiHelper.js

--- web/utils/apiHelper.js
+++ web/utils/apiHelper.js
@@ -1,37 +1,62 @@
-import axios from 'axios';
+import axios from "axios";
 
+const API = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  withCredentials: true, // IMPORTANT for cookies/auth
+});
+
+// AUTH GET (token optional)
 export const getAuth = async (url, token) => {
-  const response = await axios
-    .get(url, { headers: { Authorization: `Bearer ${token}` } })
-    .catch((err) => err.response);
-
-  return response.data;
+  try {
+    const response = await API.get(url, {
+      headers: token ? { Authorization: `Bearer ${token}` } : undefined,
+    });
+    return response.data;
+  } catch (err) {
+    return err.response?.data;
+  }
 };
 
+// AUTH POST
 export const postAuth = async (data, url, token) => {
-  const response = await axios
-    .post(url, data, { headers: { Authorization: `Bearer ${token}` } })
-    .catch((err) => err.response);
-
-  return response.data;
+  try {
+    const response = await API.post(url, data, {
+      headers: token ? { Authorization: `Bearer ${token}` } : undefined,
+    });
+    return response.data;
+  } catch (err) {
+    return err.response?.data;
+  }
 };
 
+// NORMAL POST
 export const post = async (data, url) => {
-  const response = await axios.post(url, data).catch((err) => err.response);
-
-  return response.data;
+  try {
+    const response = await API.post(url, data);
+    return response.data;
+  } catch (err) {
+    return err.response?.data;
+  }
 };
 
+// NORMAL GET
 export const get = async (url) => {
-  const response = await axios.get(url).catch((err) => err.response);
-
-  return response.data;
+  try {
+    const response = await API.get(url);
+    return response.data;
+  } catch (err) {
+    return err.response?.data;
+  }
 };
 
+// AUTH PATCH
 export const patchAuth = async (data, url, token) => {
-  const response = await axios
-    .patch(url, data, { headers: { Authorization: `Bearer ${token}` } })
-    .catch((err) => err.response);
-
-  return response.data;
+  try {
+    const response = await API.patch(url, data, {
+      headers: token ? { Authorization: `Bearer ${token}` } : undefined,
+    });
+    return response.data;
+  } catch (err) {
+    return err.response?.data;
+  }
 };
